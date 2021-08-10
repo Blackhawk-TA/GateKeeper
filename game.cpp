08d53ad7d8ddc19aa4129e13c15ed2b4f9118dec
Blackhawk <daniel.peters1@pm.me>
2021-08-10 23:58:13 +0200

    Set project configuration

--- game.cpp
+++ game.cpp
@@ -6,38 +6,28 @@ using namespace blit;
 //
 // init()
 //
-// setup your game here
+// This is called when starting the game.
 //
 void init() {
-    set_screen_mode(ScreenMode::hires);
+	set_screen_mode(ScreenMode::hires);
 }
 
 ///////////////////////////////////////////////////////////////////////////
 //
 // render(time)
 //
-// This function is called to perform rendering of the game. time is the 
+// This function is called to perform rendering of the game. time is the
 // amount if milliseconds elapsed since the start of your game
 //
 void render(uint32_t time) {
-
-    // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
-    screen.clear();
-
-    // draw some text at the top of the screen
-    screen.alpha = 255;
-    screen.mask = nullptr;
-    screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
-    screen.pen = Pen(0, 0, 0);
-    screen.text("Hello 32blit!", minimal_font, Point(5, 4));
+	screen.clear();
 }
 
 ///////////////////////////////////////////////////////////////////////////
 //
 // update(time)
 //
-// This is called to update your game state. time is the 
+// This is called to update your game state. time is the
 // amount if milliseconds elapsed since the start of your game
 //
 void update(uint32_t time) {
