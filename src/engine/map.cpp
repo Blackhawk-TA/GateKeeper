9e6ee4d07653c4ea0bb289ae0f19c7b61e6e69f1
Blackhawk <daniel.peters1@pm.me>
2021-09-11 21:42:13 +0200

    Store first position of tile instead of last for TileMap

--- src/engine/map.cpp
+++ src/engine/map.cpp
@@ -22,15 +22,15 @@ std::vector<map::Tile> tile_data;
  * @param map_section The enum describing the map section
  */
 void map::load_section(MapSections map_section) {
-	// Remove old TileMap from memory to load_section a new one
+	//Remove old TileMap from memory to load_section a new one
 	if (tmx != nullptr) {
 		free(tmx);
 	}
 
-	// Remove old TileMap from layer_data
-	layer_data.clear();
+	//Remove old TileMap
+	tile_data.clear();
 
-	// Allocate memory for TileMap and copy it into memory
+	//Allocate memory for TileMap and copy it into memory
 	switch (map_section) {
 		case MapSections::DUNGEON:
 			tmx = nullptr;
@@ -60,62 +60,59 @@ void map::load_section(MapSections map_section) {
 
 	//TODO move to own function and class with Tile and SpriteData structs
 	bool first_tile = true;
-	bool last_tile = false;
+	bool last_tile;
 	uint16_t tile_id;
 	uint16_t previous_tile_id = 0; //Set to 0, so it can never be equal to tile_id on first run. Prevents first tile being count as 2
 	uint16_t range = 0;
 	uint16_t x, y, z;
-	uint8_t next_tile_x;
-	uint8_t next_tile_y;
+	uint8_t previous_tile_x = 0;
+	uint8_t previous_tile_y = 0;
 
-	//TODO get rid of triple for loop
+	//TODO get rid of triple for loop, maybe by multiplicating them all
 	for (z = 0u; z < tmx->layers; z++) {
 		for (x = 0u; x < tmx->width; x++) {
 			for (y = 0u; y < tmx->height; y++) {
 				//Extract current tile id from tmx data
+				//TODO umbauen so dass es nicht von oben nach unten sondern von rechts nach links liest.
 				tile_id = tmx->data[y * tmx->width + x + z * LEVEL_SIZE];
 
-				//Check if it is the last tile, if so skip all other steps and save previous valid tile
-				if ((tmx->layers - 1) * (tmx->width - 1) * (tmx->height - 1) == x * y * z) {
-					last_tile = true;
-				}
+				last_tile = (tmx->layers - 1) * (tmx->width - 1) * (tmx->height - 1) == x * y * z;
 
-				//Do not save empty tiles
-				if (tile_id == tmx->empty_tile && !last_tile) {
-					continue;
-				}
-
-				//Instead of saving each tile count repetitions
-				if (tile_id == previous_tile_id && !last_tile) {
-					range++;
-					continue;
-				}
-
-				//For first tile, set previous tile to current one
-				if (first_tile) {
-					previous_tile_id = tile_id;
-					first_tile = false;
-					continue;
-				}
-
+				//Check if this is not the last tile. If it is the last, skip all steps and save previous
 				if (!last_tile) {
-					next_tile_x = x;
-					next_tile_y = y;
+					//Do not save empty tiles
+					if (tile_id == tmx->empty_tile) {
+						continue;
+					}
+
+					//For first tile, set previous tile to current one
+					if (first_tile) {
+						previous_tile_id = tile_id;
+						first_tile = false;
+						continue;
+					}
+
+					//Instead of saving each tile count repetitions
+					if (tile_id == previous_tile_id) {
+						range++;
+						continue;
+					}
 				}
 
-				//TODO previous tile gets x and y pos if next tile...
-				//Save last tile in row of equals and its position. Requires calculation position of previous tiles
-				tile_data.push_back(Tile{ //TODO last tile missing or first tile missing if using tile_id
-					next_tile_x,
-					next_tile_y,
+				//Save first tile in row of equals and its position.
+				tile_data.push_back(Tile{
+					previous_tile_x,
+					previous_tile_y,
 					previous_tile_id,
 					range,
 					static_cast<uint16_t>((previous_tile_id % sprite_sheet_size.x) * TILE_SIZE),
 					static_cast<uint16_t>((previous_tile_id / sprite_sheet_size.y) * TILE_SIZE),
 				});
 
-				//Reset range information for next tile with different id
+				//Reset range information for next tile with a different id
 				range = 0;
+				previous_tile_x = x;
+				previous_tile_y = y;
 				previous_tile_id = tile_id;
 			}
 		}
@@ -159,21 +156,29 @@ void map::draw(Point camera_position) {
 
 	uint16_t i, tile_x, tile_y;
 	for (Tile &tile : tile_data) {
-		tile_x = tile.x;
-		tile_y = tile.y;
+		if (tile.range > 0) {
+			for (i = 0u; i < tile.range; i++) {
+				tile_x = (tile.x + i) % tmx->width; //TODO should render from top to button before doing left to right
+				tile_y = (tile.y + i) / tmx->width;
 
-		for (i = 0u; i < tile.range; i++) {
-			tile_x = (tile_x - 1) % (tmx->width - 1);
-			tile_y = ((tile_y - 1)) / (tmx->height - 1);
-
+				//Checks if tile is visible on screen
+				if (screen_tiles.x + camera_position_world.x - tile_x >= 0 && camera_position_world.x <= tile_x &&
+				    screen_tiles.y + camera_position_world.y - tile_y >= 0 && camera_position_world.y <= tile_y) {
+					screen.blit_sprite(
+							Rect(tile.sprite_rect_x, tile.sprite_rect_y, TILE_SIZE, TILE_SIZE),
+							Point(tile_x * TILE_SIZE, tile_y * TILE_SIZE) - camera_position,
+							SpriteTransform::NONE
+					);
+				}
+			}
+		} else {
 			//Checks if tile is visible on screen
 			if (screen_tiles.x + camera_position_world.x - tile_x >= 0 && camera_position_world.x <= tile_x &&
-				screen_tiles.y + camera_position_world.y - tile_x >= 0 && camera_position_world.y <= tile_y)
-			{
+			    screen_tiles.y + camera_position_world.y - tile_x >= 0 && camera_position_world.y <= tile_y) {
 				screen.blit_sprite(
-					Rect(tile.sprite_rect_x, tile.sprite_rect_y, TILE_SIZE, TILE_SIZE),
-					Point(tile_x * TILE_SIZE, tile_y * TILE_SIZE) - camera_position,
-					SpriteTransform::NONE
+						Rect(tile.sprite_rect_x, tile.sprite_rect_y, TILE_SIZE, TILE_SIZE),
+						Point(tile.x * TILE_SIZE, tile.y * TILE_SIZE) - camera_position,
+						SpriteTransform::NONE
 				);
 			}
 		}
