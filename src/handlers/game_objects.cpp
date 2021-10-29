660f2da919c749c9eb87a4241091143639ea6d98
Blackhawk <daniel.peters1@pm.me>
2021-10-29 19:19:44 +0200

    Fix game object collection

--- src/handlers/game_objects.cpp
+++ src/handlers/game_objects.cpp
@@ -5,10 +5,10 @@
 #include "game_objects.hpp"
 #include "../game_objects/gate_statue.hpp"
 
-std::vector<IGameObject> *game_object_collection;
+std::vector<IGameObject*> game_object_collection;
 
 void game_objects::init() {
-	game_object_collection->emplace_back(GateStatue(map::DUNGEON, Point(14, 24), false));
+	game_object_collection.emplace_back(new GateStatue(map::DUNGEON, Point(14, 22), false));
 }
 
 std::vector<IGameObject> game_objects::generate_game_objects(std::vector<Point> &positions, map::MapSections map_section) {
@@ -24,8 +24,8 @@ void game_objects::save() {
 }
 
 void game_objects::draw() {
-	for (auto &game_object : *game_object_collection) {
-		game_object.draw();
+	for (auto &game_object : game_object_collection) {
+		game_object->draw();
 	}
 }
 
@@ -33,8 +33,8 @@ bool game_objects::check_collisions(Point next_position) {
 	bool collision = false;
 	uint16_t i = 0;
 
-	while (!collision && i < game_object_collection->size()) {
-		collision = game_object_collection->at(i).check_collision(next_position);
+	while (!collision && i < game_object_collection.size()) {
+		collision = game_object_collection.at(i)->check_collision(next_position);
 		i++;
 	}
 
@@ -45,8 +45,8 @@ bool game_objects::interact() {
 	bool interacted = false;
 	uint16_t i = 0;
 
-	while (!interacted && i < game_object_collection->size()) {
-		interacted = game_object_collection->at(i).interact();
+	while (!interacted && i < game_object_collection.size()) {
+		interacted = game_object_collection.at(i)->interact();
 		i++;
 	}
 
@@ -54,7 +54,7 @@ bool game_objects::interact() {
 }
 
 void game_objects::update_states(Point next_position) {
-	for (auto &game_object : *game_object_collection) {
-		game_object.update_state(next_position);
+	for (auto &game_object : game_object_collection) {
+		game_object->update_state(next_position);
 	}
 }
