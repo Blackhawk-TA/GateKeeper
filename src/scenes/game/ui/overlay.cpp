bb26d4f237456d49feda39f570ca697d8f5e7728
Blackhawk <daniel.peters1@pm.me>
2021-11-06 16:57:44 +0100

    Move status bar to the top

--- src/scenes/game/ui/overlay.cpp
+++ src/scenes/game/ui/overlay.cpp
@@ -9,9 +9,9 @@ using namespace blit;
 
 void overlay::draw_statusbar(uint8_t health) {
 	screen.pen = Pen(255, 255, 255, 200);
-	screen.rectangle(Rect(0, screen.bounds.h - 10, screen.bounds.w, 10));
+	screen.rectangle(Rect(0, 0, screen.bounds.w, 10));
 
 	screen.pen = Pen(0, 0, 0, 255);
 	std::string health_string = "Health: " + std::to_string(health);
-	screen.text(health_string, minimal_font, Rect(screen.bounds.w - 10, screen.bounds.h - 10, 10, 10), false, TextAlign::right);
+	screen.text(health_string, minimal_font, Rect(3, 1, 10, 10), false, TextAlign::left);
 }
