d7773f50b52f3fff18e6b3d83fb6efd18acabab0
Blackhawk <daniel.peters1@pm.me>
2021-11-08 00:00:58 +0100

    Fix items not saving correctly

--- src/utils/saves/savegame.cpp
+++ src/utils/saves/savegame.cpp
@@ -17,7 +17,7 @@ std::array<savegame::Item, listbox_item::INVENTORY_COUNTER> compress_items(std::
 	std::array<savegame::Item, listbox_item::INVENTORY_COUNTER> compressed_items = {};
 	uint8_t amount;
 
-	for (auto i = 0u; i < listbox_item::INVENTORY_COUNTER; i++) {
+	for (auto i = 0u; i < items.size(); i++) {
 		amount = i < items.size() ? items[i].amount : 0;
 		compressed_items[i] = savegame::Item{
 			items[i].type,
@@ -36,7 +36,7 @@ std::vector<Listbox::Item> decompress_items(std::array<savegame::Item, listbox_i
 		item_template = listbox_item::create_inventory_item(static_cast<listbox_item::INVENTORY_ITEM>(item.type));
 
 		//Include only items that have an amount > 0 or are a menu item
-		if (item.amount > 0 || (item.amount == 0 && !item_template.single_use)) {
+		if (item.type > 0 && (item.amount > 0 || (item.amount == 0 && !item_template.single_use))) {
 			decompressed_items.emplace_back(Listbox::Item{
 				item.type,
 				item_template.name,
