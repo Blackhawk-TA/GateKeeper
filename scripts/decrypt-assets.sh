6475783b813237daaa7ce26447ea34c89016a0ed
Blackhawk <daniel.peters1@pm.me>
2021-12-27 02:03:31 +0100

    Add decrypt assets helper scripts

---
+++ scripts/decrypt-assets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/characters.png -o ../assets/characters.png --key=ASSET_KEY
+python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/player_attack.png -o ../assets/player_attack.png --key=ASSET_KEY
+python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/spritesheet.png -o ../assets/spritesheet.png --key=ASSET_KEY
