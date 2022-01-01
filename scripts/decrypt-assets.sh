5eb441c04aede2f62803f97907639b5f0d2fff7c
Blackhawk <daniel.peters1@pm.me>
2022-01-01 23:10:53 +0100

    Update assets

--- scripts/decrypt-assets.sh
+++ scripts/decrypt-assets.sh
@@ -3,3 +3,4 @@
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/characters.png -o ../assets/characters.png --key=ASSET_KEY
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/player_attack.png -o ../assets/player_attack.png --key=ASSET_KEY
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/spritesheet.png -o ../assets/spritesheet.png --key=ASSET_KEY
+python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/knight_attack.png -o ../assets/knight_attack.png --key=ASSET_KEY
--- scripts/encrypt-assets.sh
+++ scripts/encrypt-assets.sh
@@ -3,3 +3,4 @@
 python3 asset_helper.py --mode=encrypt -i ../assets/characters.png -o ../assets/encrypted/characters.png --key=ASSET_KEY
 python3 asset_helper.py --mode=encrypt -i ../assets/player_attack.png -o ../assets/encrypted/player_attack.png --key=ASSET_KEY
 python3 asset_helper.py --mode=encrypt -i ../assets/spritesheet.png -o ../assets/encrypted/spritesheet.png --key=ASSET_KEY
+python3 asset_helper.py --mode=encrypt -i ../assets/knight_attack.png -o ../assets/encrypted/knight_attack.png --key=ASSET_KEY
