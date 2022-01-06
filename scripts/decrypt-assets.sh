045f0d267ffc0abf3989f8a6a14263e690efd6cf
Blackhawk <daniel.peters1@pm.me>
2022-01-06 23:07:07 +0100

    Add weapon sprites

--- scripts/decrypt-assets.sh
+++ scripts/decrypt-assets.sh
@@ -3,3 +3,4 @@
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/characters.png -o ../assets/characters.png --key=ASSET_KEY
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/spritesheet.png -o ../assets/spritesheet.png --key=ASSET_KEY
 python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/attack_animations.png -o ../assets/attack_animations.png --key=ASSET_KEY
+python3 asset_helper.py --mode=decrypt -i ../assets/encrypted/weapons.png -o ../assets/weapons.png --key=ASSET_KEY
--- scripts/encrypt-assets.sh
+++ scripts/encrypt-assets.sh
@@ -3,3 +3,4 @@
 python3 asset_helper.py --mode=encrypt -i ../assets/characters.png -o ../assets/encrypted/characters.png --key=ASSET_KEY
 python3 asset_helper.py --mode=encrypt -i ../assets/spritesheet.png -o ../assets/encrypted/spritesheet.png --key=ASSET_KEY
 python3 asset_helper.py --mode=encrypt -i ../assets/attack_animations.png -o ../assets/encrypted/attack_animations.png --key=ASSET_KEY
+python3 asset_helper.py --mode=encrypt -i ../assets/weapons.png -o ../assets/encrypted/weapons.png --key=ASSET_KEY
