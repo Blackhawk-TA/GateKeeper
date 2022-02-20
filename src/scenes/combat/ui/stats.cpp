22886068424522386f3e5434a71656b8c33bf04c
Blackhawk <daniel.peters1@pm.me>
2022-02-20 16:44:36 +0100

    Improve combat stats overlay

--- src/scenes/combat/ui/stats.cpp
+++ src/scenes/combat/ui/stats.cpp
@@ -8,28 +8,34 @@
 
 namespace combat {
 	void stats::draw(uint8_t health, uint8_t stamina, uint8_t enemy_health) {
-		//Background box
+		//Background box left
 		screen.pen = Pen(0, 0, 0, 150);
-		screen.rectangle(Rect(0, 0, 160, 30));
-
-		//Text and Statusbar background
-		for (uint8_t i = 0u; i < sizeof(STATUS_BARS) / sizeof(STATUS_BARS[0]); i++) {
-			screen.pen = Pen(255, 255, 255, 255);
-			screen.text(STATUS_BARS[i], minimal_font, Rect(3, 2 + i * TEXT_DISTANCE, 10, 5), false, TextAlign::left);
-			screen.pen = Pen(255, 255, 255, 200);
-			screen.rectangle(Rect(50, 4 + i * TEXT_DISTANCE, BAR_LENGTH, 2));
-		}
-
-		// Player health
-		screen.pen = Pen(0, 255, 0, 255);
-		screen.rectangle(Rect(50, 4, int(float(health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
-
-		//Player stamina
-		screen.pen = Pen(0, 215, 180, 255);
-		screen.rectangle(Rect(50, 4 + TEXT_DISTANCE, int(float(stamina) / float(Player::MAX_STAMINA) * BAR_LENGTH), 2));
+		screen.rectangle(Rect(0, BOX_Y_POS, BOX_WIDTH, 22));
+		screen.pen = Pen(255, 255, 255, 255);
+		screen.text("Enemy Health", minimal_font, Rect(0, BOX_Y_POS - 7, 10, 5), false, TextAlign::left);
 
 		//Enemy health
+		screen.pen = Pen(255, 255, 255, 200);
+		screen.rectangle(Rect(5, BOX_Y_POS + 10, BAR_LENGTH, 2));
 		screen.pen = Pen(255, 0, 0, 255);
-		screen.rectangle(Rect(50, 4 + 2 * TEXT_DISTANCE, int(float(enemy_health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
+		screen.rectangle(Rect(5, BOX_Y_POS + 10, int(float(enemy_health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
+
+		//Background box right
+		screen.pen = Pen(0, 0, 0, 150);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH, BOX_Y_POS, BOX_WIDTH, 22));
+		screen.pen = Pen(255, 255, 255, 255);
+		screen.text("Health/Stamina", minimal_font, Rect(screen.bounds.w - BOX_WIDTH, BOX_Y_POS - 7, 10, 5), false, TextAlign::left);
+
+		// Player health
+		screen.pen = Pen(255, 255, 255, 200);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 5 , BAR_LENGTH, 2));
+		screen.pen = Pen(0, 255, 0, 255);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 5, int(float(health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
+
+		//Player stamina
+		screen.pen = Pen(255, 255, 255, 200);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 5, BAR_LENGTH, 2));
+		screen.pen = Pen(0, 215, 180, 255);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 5, int(float(stamina) / float(Player::MAX_STAMINA) * BAR_LENGTH), 2));
 	}
 }
--- src/scenes/combat/ui/stats.hpp
+++ src/scenes/combat/ui/stats.hpp
@@ -7,8 +7,9 @@
 
 namespace combat::stats {
 	const uint8_t TEXT_DISTANCE = 10;
-	const uint8_t BAR_LENGTH = 107;
-	const std::string STATUS_BARS[3] = {"Health", "Stamina", "Enemy"};
+	const uint8_t BAR_LENGTH = 100;
+	const uint8_t BOX_Y_POS = 170;
+	const uint8_t BOX_WIDTH = 110;
 
 	void draw(uint8_t health, uint8_t stamina, uint8_t enemy_health);
 }
