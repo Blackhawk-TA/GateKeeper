6293dc28b2e8e837f9964357db648cef6a4a02f9
Blackhawk <daniel.peters1@pm.me>
2022-02-20 23:49:56 +0100

    Adjust combat stats overlay background size

--- src/scenes/combat/ui/stats.cpp
+++ src/scenes/combat/ui/stats.cpp
@@ -10,32 +10,32 @@ namespace combat {
 	void stats::draw(uint8_t health, uint8_t stamina, uint8_t enemy_health) {
 		//Background box left
 		screen.pen = Pen(0, 0, 0, 150);
-		screen.rectangle(Rect(0, BOX_Y_POS, BOX_WIDTH, 22));
-		screen.pen = Pen(0, 0, 0, 255);
-		screen.text("Enemy Health", minimal_font, Rect(0, BOX_Y_POS - 7, 10, 5), false, TextAlign::left);
+		screen.rectangle(Rect(0, BOX_Y_POS, BOX_WIDTH, 32));
+		screen.pen = Pen(255, 255, 255, 255);
+		screen.text("Enemy Health", minimal_font, Rect(5, BOX_Y_POS + 4, 10, 5), false, TextAlign::left);
 
 		//Enemy health
 		screen.pen = Pen(255, 255, 255, 200);
-		screen.rectangle(Rect(5, BOX_Y_POS + 10, BAR_LENGTH, 2));
+		screen.rectangle(Rect(5, BOX_Y_POS + 20, BAR_LENGTH, 2));
 		screen.pen = Pen(255, 0, 0, 255);
-		screen.rectangle(Rect(5, BOX_Y_POS + 10, int(float(enemy_health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
+		screen.rectangle(Rect(5, BOX_Y_POS + 20, int(float(enemy_health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
 
 		//Background box right
 		screen.pen = Pen(0, 0, 0, 150);
-		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH, BOX_Y_POS, BOX_WIDTH, 22));
-		screen.pen = Pen(0, 0, 0, 255);
-		screen.text("Health/Stamina", minimal_font, Rect(screen.bounds.w - BOX_WIDTH, BOX_Y_POS - 7, 10, 5), false, TextAlign::left);
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH, BOX_Y_POS, BOX_WIDTH, 32));
+		screen.pen = Pen(255, 255, 255, 255);
+		screen.text("Health/Stamina", minimal_font, Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 4, 10, 5), false, TextAlign::left);
 
 		// Player health
 		screen.pen = Pen(255, 255, 255, 200);
-		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 5 , BAR_LENGTH, 2));
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 15 , BAR_LENGTH, 2));
 		screen.pen = Pen(0, 255, 0, 255);
-		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 5, int(float(health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + 15, int(float(health) / float(Character::MAX_HEALTH) * BAR_LENGTH), 2));
 
 		//Player stamina
 		screen.pen = Pen(255, 255, 255, 200);
-		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 5, BAR_LENGTH, 2));
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 15, BAR_LENGTH, 2));
 		screen.pen = Pen(0, 215, 180, 255);
-		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 5, int(float(stamina) / float(Player::MAX_STAMINA) * BAR_LENGTH), 2));
+		screen.rectangle(Rect(screen.bounds.w - BOX_WIDTH + 5, BOX_Y_POS + TEXT_DISTANCE + 15, int(float(stamina) / float(Player::MAX_STAMINA) * BAR_LENGTH), 2));
 	}
 }
--- src/scenes/combat/ui/stats.hpp
+++ src/scenes/combat/ui/stats.hpp
@@ -8,7 +8,7 @@
 namespace combat::stats {
 	const uint8_t TEXT_DISTANCE = 10;
 	const uint8_t BAR_LENGTH = 100;
-	const uint8_t BOX_Y_POS = 170;
+	const uint8_t BOX_Y_POS = 160;
 	const uint8_t BOX_WIDTH = 110;
 
 	void draw(uint8_t health, uint8_t stamina, uint8_t enemy_health);
