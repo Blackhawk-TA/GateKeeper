4272b7cd5b4a1a48a2c2ef850a96ca9721f69455
Blackhawk <daniel.peters1@pm.me>
2022-03-17 00:57:40 +0100

    Fix typo

--- src/scenes/game/game_objects/characters/nurse.cpp
+++ src/scenes/game/game_objects/characters/nurse.cpp
@@ -15,7 +15,7 @@ namespace game {
 	bool Nurse::player_interact() {
 		if (player_usable && player_in_front(BAR_HEIGHT)) {
 			if (player_handler::get_health() == Player::MAX_HEALTH) {
-				textbox = new Textbox("Hello, I'm the nurse here. I can heal you if you want, but you seem be healthy.");
+				textbox = new Textbox("Hello, I'm the nurse here. I can heal you if you want, but you seem to be healthy.");
 			} else {
 				textbox = new Textbox("Hello, I'm the nurse here. You look injured, let me heal you!");
 				player_handler::heal(Player::MAX_HEALTH);
