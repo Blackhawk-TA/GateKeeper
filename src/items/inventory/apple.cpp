83b313ae8b1f2550f9bf56aa18f7bc08083fb282
Blackhawk <daniel.peters1@pm.me>
2022-04-01 21:58:15 +0200

    Fix food heal amount

--- src/items/inventory/apple.cpp
+++ src/items/inventory/apple.cpp
@@ -6,17 +6,18 @@
 #include "../../scenes/game/game_objects/handler/player_handler.hpp"
 
 Listbox::Item items::create_apple(uint8_t type_id) {
+	uint8_t heal_amount = 100;
 	return Listbox::Item{
 		type_id,
 		"Apple",
-		"Regenerates 70 health when eaten.",
+		"Regenerates " + std::to_string(heal_amount) + " health when eaten.",
 		"You ate an apple.",
 		"You're not hungry.",
 		true,
 		1,
-		[] {
+		[heal_amount] {
 			if (game::player_handler::get_health() < 100) {
-				game::player_handler::heal(100);
+				game::player_handler::heal(heal_amount);
 				return Listbox::Tooltip::SUCCESS;
 			} else {
 				return Listbox::Tooltip::FAILURE;
--- src/items/inventory/carrot.cpp
+++ src/items/inventory/carrot.cpp
@@ -6,17 +6,18 @@
 #include "../../scenes/game/game_objects/handler/player_handler.hpp"
 
 Listbox::Item items::create_carrot(uint8_t type_id) {
+	uint8_t heal_amount = 75;
 	return Listbox::Item{
 		type_id,
 		"Carrot",
-		"Regenerates 50 health when eaten.",
+		"Regenerates " + std::to_string(heal_amount) + " health when eaten.",
 		"You ate a carrot.",
 		"You're not hungry.",
 		true,
 		1,
-		[] {
+		[heal_amount] {
 			if (game::player_handler::get_health() < 100) {
-				game::player_handler::heal(75);
+				game::player_handler::heal(heal_amount);
 				return Listbox::Tooltip::SUCCESS;
 			} else {
 				return Listbox::Tooltip::FAILURE;
