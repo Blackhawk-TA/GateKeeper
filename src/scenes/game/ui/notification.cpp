11d8d97c299fb97d7a779d7dd592b1aa8dff2ca5
Blackhawk <daniel.peters1@pm.me>
2022-04-10 16:18:23 +0200

    Improve notification readability

--- src/scenes/game/ui/notification.cpp
+++ src/scenes/game/ui/notification.cpp
@@ -46,8 +46,12 @@ namespace game::notification {
 		if (current_notification == nullptr) return;
 
 		if (!current_notification->message.empty()) { //Message notification
-			screen.pen = Pen(255, 255, 255, 255);
-			screen.text(current_notification->message, minimal_font, Rect(1, 13, 10, 10), false, TextAlign::left);
+			Size size = screen.measure_text(current_notification->message, minimal_font);
+			screen.pen = Pen(255, 255, 255, 200);
+			screen.rectangle(Rect(0, 12, size.w + 15, size.h));
+			screen.triangle(Point(size.w + 14, 11), Point(size.w + 14, 21), Point(size.w + 24, 11));
+			screen.pen = Pen(0, 0, 0, 255);
+			screen.text(current_notification->message, minimal_font, Rect(1, 13, size.w, size.h), false, TextAlign::left);
 		} else if (current_notification->tile_id != NO_ICON) { //Icon notification
 			Size spritesheet_size = get_spritesheet_size(screen.sprites->bounds);
 			screen.blit(
